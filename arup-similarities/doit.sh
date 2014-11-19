821fbb044502349c176924a17bbb1a8e5b079aa5
Alex Henrie <alexhenrie24@gmail.com>
2014-11-19 16:46:54 -0700

    Expand similarity comparisons

--- arup-similarities/doit.sh
+++ arup-similarities/doit.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-#Compares ARUP's set of Noonan phenotypes to MEDGEN's set of Noonan phenotypes
-java -jar ../sml-toolkit-0.9.jar -t sm -xmlconf sml-xmlconf-hpo.xml
+java -jar ../slib/slib-tools/slib-tools-sml-toolkit/target/sml-toolkit-latest.jar -t sm -xmlconf sml-xmlconf-hpo.xml
 cat output.tsv
