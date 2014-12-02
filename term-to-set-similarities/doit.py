2ea3985d20ea79f430aa27519871319acc034710
Alex Henrie <alexhenrie24@gmail.com>
2014-12-01 17:35:01 -0700

    Compare terms to sets by finding the best term-to-term match in the set

--- term-to-set-similarities/doit.py
+++ term-to-set-similarities/doit.py
@@ -1,49 +1,64 @@
 #!/bin/python3
+#compares all interesting terms to each set, returning the best SimUI score between the term and each of the terms in the set
 
 import os
 import re
 import subprocess
 
-total_output = '\tsimui\n'
-total_output += 'term\tgold\tmedgen\tarup\tmayo\n'
+#define which sets we care about
+set_names = ['gold', 'medgen', 'arup', 'mayo']
 
-terms = set()
+#write the header
+total_output = '\tbest term-to-term simui\nterm'
+for set_name in set_names:
+    total_output += '\t' + set_name
+total_output += '\n'
+
+#parse the sets of terms
+term_sets = dict()
+term_sets['all'] = set()
 for line in open('../set_instance_annots.tsv'):
     line = line.rstrip()
     cells = line.split('\t')
     if len(cells) == 1:
         continue
-    terms |= set(cells[1].split(';'))
+    term_sets[cells[0]] = set(cells[1].split(';'))
+    term_sets['all'] |= term_sets[cells[0]]
 
-terms = sorted(list(terms))
+#sort the set of all terms
+term_sets['all'] = sorted(list(term_sets['all']))
 
+#write an annotation file giving each term an ID
 annots_file = open('instance_annots.tsv', 'w')
 annots_file.write(open('../set_instance_annots.tsv').read() + '\n')
-for term in terms:
+for term in term_sets['all']:
     annots_file.write(term.replace(':', '') + '\t' + term + '\n')
 annots_file.close()
 
-queries_file = open('input_queries.tsv', 'w')
-for term in terms:
-    term_id = term.replace(':', '')
-    queries_file.write(term_id + '\tgold\n')
-    queries_file.write(term_id + '\tmedgen\n')
-    queries_file.write(term_id + '\tarup\n')
-    queries_file.write(term_id + '\tmayo\n')
-queries_file.close()
+#compare the terms
+for term in term_sets['all']:
+    scores = dict()
+    for set_name in set_names:
+        queries_file = open('input_queries.tsv', 'w')
+        for set_term in term_sets[set_name]:
+            queries_file.write(term.replace(':', '') + '\t' + set_term.replace(':', '') + '\n')
+        queries_file.close()
+        subprocess.call('java -jar ../slib/slib-tools/slib-tools-sml-toolkit/target/sml-toolkit-latest.jar -t sm -xmlconf sml-xmlconf-hpo.xml', shell=True)
 
-subprocess.call('java -jar ../slib/slib-tools/slib-tools-sml-toolkit/target/sml-toolkit-latest.jar -t sm -xmlconf sml-xmlconf-hpo.xml', shell=True)
-output_file_contents = open('output.tsv').read()
+        best_score = 0;
+        for match in re.finditer(r'\t([0-9.]+)', open('output.tsv').read()):
+            score = float(match.group(1))
+            if score > best_score:
+                best_score = score
 
-for term in terms:
-    term_id = term.replace(':', '')
-    gold = re.compile(term_id + r'\tgold\t([0-9.]+)').search(output_file_contents)
-    medgen = re.compile(term_id + r'\tmedgen\t([0-9.]+)').search(output_file_contents)
-    arup = re.compile(term_id + r'\tarup\t([0-9.]+)').search(output_file_contents)
-    mayo = re.compile(term_id + r'\tmayo\t([0-9.]+)').search(output_file_contents)
-    if gold and medgen and arup and mayo:
-        total_output += term + '\t' + gold.group(1) + '\t' + medgen.group(1) + '\t' + arup.group(1) + '\t' + mayo.group(1) + '\n'
+        scores[set_name] = best_score
 
+    total_output += term
+    for set_name in set_names:
+        total_output += '\t' + str(scores[set_name])
+    total_output += '\n'
+
+#clean up
 os.remove('instance_annots.tsv')
 os.remove('input_queries.tsv')
 os.remove('output.tsv')
